e876267d287da39140240a531a7d4ca2e6dd1adf
Antje Lemberg <a.h.m.lemberg@gmail.com>
2018-06-28 13:36:16 +0200

    Add two cats with separate counters

--- js/app.js
+++ js/app.js
@@ -1,15 +1,29 @@
 $(function() {
-  let counterValue = 0;
 
-$(".hero-image").click(function() {
-  counterValue += 1;
-  $('.counter').text(counterValue);
+
+
+$(".individual-cat").click(function(event) {
+  counter = $(this).find('.counter');
+  let counterValue = Number(counter.text());
+  counter.text(function() {
+    let newCounterValue = counterValue + 1;
+    return newCounterValue;
+  });
+
+  })
 
 })
 
 
 
 
+class Cat {
+  constructor(name, src) {
+    this.name = name;
+    this.src = src;
+  }
 
-})
+}
+
+
 //end document ready
