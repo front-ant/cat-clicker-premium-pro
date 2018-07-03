faae58e41351c42facb0d32784bb2b7ba104ee1f
Antje Lemberg <a.h.m.lemberg@gmail.com>
2018-07-03 21:05:53 +0200

    Try to put all the functionality into the three main objects

--- js/app.js
+++ js/app.js
@@ -6,58 +6,81 @@ let catNames = [
   'Cafe'
 ];
 
-let Cat = '';
 let constructedCats = new Map();
-let catToBeDisplayed = ""; // References currently active instance of Cat()
 
 //when DOM is ready:
 $(function() {
 
+const model = {
+  Cat: class Cat {
+    constructor(name) {
+      this.name = name;
+      this.clicked = 0;
+    };
+  },
 
-Cat = class Cat {
-  constructor(name) {
-  this.name = name;
-  this.clicked = 0;
+  constructCats: function() {
+    for (let i = 0; i < catNames.length; i++) {
+      constructedCats.set(catNames[i], new Cat(catNames[i]));
+    };
   }
 
-  display() {
-    $('.cat-container').html(
-      this.name +
-      `<img src='img\\${this.name}.jpg' height="300">` +
-      `Click Count: ${this.clicked}`
-    );
-  }
 
-  updateClickCount() {
-    this.clicked += 1;
+};
+
+const view1 = {
+  display: function() {
+    for (let i = 0; i < catNames.length; i++) {
+       $('.catPicker').append(`<li class='catName'>${catNames[i]}</li>`);
   }
 }
+};
 
-// Loop over catNames array to create content for catPicker menu
-// and put the instantiated cat objects into the constructedCats map.
-// Assign the cat's names as key for the cat object values
-for (let i = 0; i < catNames.length; i++) {
-   $('.catPicker').append(`<li class='catName'>${catNames[i]}</li>`);
-  constructedCats.set(catNames[i], new Cat(catNames[i]));
-}
+const view2 = {
 
+};
 
-//event listener for menu clicks
-
-$('.catName').click(function(event) {
-  let clickedCat = event.target;
-  let clickedCatName = clickedCat.innerHTML;
-  // grab active cat object. The variable is also used when
-  // the displayed cat picture is clicked later
-  catToBeDisplayed = constructedCats.get(clickedCatName)
-  if (clickedCat.nodeName === 'LI') {
-     catToBeDisplayed.display();
-     catToBeDisplayed.updateClickCount(); // maybe not necessary
+const octopus = {
+  init: function() {
+    model.constructCats();
+    view1.display();
   }
-});
+};
 
-$('.cat-container').click(function(event) {
-  catToBeDisplayed.display();
-  catToBeDisplayed.updateClickCount()
-  });
+//
+//   display() {
+//     $('.cat-container').html(
+//       this.name +
+//       `<img src='img\\${this.name}.jpg' height="300">` +
+//       `Click Count: ${this.clicked}`
+//     );
+//   }
+//
+//   updateClickCount() {
+//     this.clicked += 1;
+//   }
+// }
+//
+//
+// //event listener for menu clicks
+//
+// $('.catName').click(function(event) {
+//   let clickedCat = event.target;
+//   let clickedCatName = clickedCat.innerHTML;
+//   // grab active cat object. The variable is also used when
+//   // the displayed cat picture is clicked later
+//   catToBeDisplayed = constructedCats.get(clickedCatName)
+//   if (clickedCat.nodeName === 'LI') {
+//      catToBeDisplayed.display();
+//      catToBeDisplayed.updateClickCount(); // maybe not necessary
+//   }
+// });
+//
+// $('.cat-container').click(function(event) {
+//   catToBeDisplayed.display();
+//   catToBeDisplayed.updateClickCount()
+//   });
+
+octopus.init();
+
 });
